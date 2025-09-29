f00d2bbf674c3d9e6b5dba1f791a32a852e9a16f
ShiitakeMushroomCode <202144092@itc.ac.kr>
2025-09-29 15:34:48 +0900

    .gitignore

--- week06/types/main.go
+++ week06/types/main.go
@@ -7,13 +7,6 @@ import (
 
 func main(){
 	
-	var float_d float64
-	var int_d int
-	var bool_d bool
-	var str_d string
-
-	fmt.Println(float_d, reflect.TypeOf(float_d))
-	fmt.Println(int_d, reflect.TypeOf(int_d))
-	fmt.Println(bool_d, reflect.TypeOf(bool_d))
-	fmt.Println(str_d, reflect.TypeOf(str_d))
+	totalPrice := 1000
+	fmt.Println(totalPrice, reflect.TypeOf(totalPrice))
 }
