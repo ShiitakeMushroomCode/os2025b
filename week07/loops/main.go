49f652036a9cb0fd1fdcc5e78aee2e04d786525c
ShiitakeMushroomCode <202144092@itc.ac.kr>
2025-10-13 15:23:41 +0900

    loops/if2

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main(){
-	// var int int = 99
-	// vat b int = 8
-	// var fmt string = "1"
-	fmt.Println()
+	fmt.Print("Enter a grade: ")
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil{
+		log.Fatal(err) //report the error, exit the program
+	}
+
+	input = strings.TrimSpace(input)
+	score, err := strconv.ParseFloat(input, 64)
+	if err != nil{
+		log.Fatal(err) //report the error, exit the program
+	}
+	fmt.Print(score)
+	if score > 60{
+		fmt.Println(" Good")
+	} else{
+		fmt.Println(" Fail")
+	}
 }
