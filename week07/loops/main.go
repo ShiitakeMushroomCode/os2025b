1ae63c5dfc08d90d7d581fd30abd99209bfa3027
ShiitakeMushroomCode <202144092@itc.ac.kr>
2025-10-13 15:33:30 +0900

    loops/rand

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -1,31 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
+	"math/rand"
 )
 
 func main(){
-	fmt.Print("Enter a grade: ")
-	reader := bufio.NewReader(os.Stdin)
-	input, err := reader.ReadString('\n')
-	if err != nil{
-		log.Fatal(err) //report the error, exit the program
-	}
-
-	input = strings.TrimSpace(input)
-	score, err := strconv.ParseFloat(input, 64)
-	if err != nil{
-		log.Fatal(err) //report the error, exit the program
-	}
-	fmt.Print(score)
-	if score > 60{
-		fmt.Println(" Good")
-	} else{
-		fmt.Println(" Fail")
-	}
+	dice := rand.Intn(6) + 1
+	fmt.Println(dice)
 }
