00d319ec4922812a6d5ff14ff1b8536f12b22676
ShiitakeMushroomCode <202144092@itc.ac.kr>
2025-10-13 14:21:37 +0900

    loops/time

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"strings"
+	"time"
 )
 
 func main(){
-	var length float64 = 3.2;
-	var width int = 2;
-	fmt.Println("Area is", int(length) *(width));
-	fmt.Println("length > width", int(length) > (width));
-	fmt.Println("형변환", reflect.TypeOf(int(length)));
-	fmt.Println("원본", reflect.TypeOf(length));
+	var now time.Time = time.Now()
+	var year int = now.Year()
+	// month := now.Month()
+	var month time.Month = now.Month()
+	var day int = now.Day()
+	fmt.Println(year)
+	fmt.Println(month)
+	fmt.Println(day)
+
+	univ := "Go$ Inha$"
+	changer := strings.NewReplacer("$", "!")
+	fixed := changer.Replace(univ)
+	fmt.Println(fixed)
 }
