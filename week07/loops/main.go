7a8a65d027852329168e002860e510032c93435b
ShiitakeMushroomCode <202144092@itc.ac.kr>
2025-10-13 15:00:15 +0900

    loops/if

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -11,6 +11,8 @@ func main(){
 	fmt.Print("Enter a grade: ")
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
+	if err != nil{
+		log.Fatal(err) //report the error, exit the program
+	}
 	fmt.Println(input)
-	log.Fatal(err) //report the error, exit the program
 }
