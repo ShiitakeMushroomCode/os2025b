5c3050aa26f114412bf20584ee5a7f64b00783d8
ShiitakeMushroomCode <202144092@itc.ac.kr>
2025-10-27 15:21:43 +0900

    func/input

--- week09/func/main.go
+++ week09/func/main.go
@@ -1,28 +1,43 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
+
 func main(){
-	f:=123
-	s:=456
-  Swap(f,s)
-	fmt.Println("Main Value\nfirst :",f,"second :",s,"\n ")
-  Pswap(&f,&s)
-	fmt.Println("Main Value\nfirst :",f,"second :",s)
+	fmt.Print("점수값을 입력하세요(0~100점 사이의 실수) : ")
+	score, err := GetFloat()
+	if err!=nil{
+		log.Fatal(err)
+	} 
+	if score < 0 || score > 100{
+		log.Fatal("잘못된 값이 입력되었습니다. 입력값 :", score)
+	}
+	status := ""
+	if score >= 90{
+		status="합격"
+	}else{
+		status="불합격"
+	}
+	fmt.Printf("%.2f점 %s입니다.", score, status)
 }
 
-func Swap(f int, s int){
-	temp:=f
-	f=s
-	s=temp
-	fmt.Println("Value Swap\nfirst :",f,"second :",s)
-}
+func GetFloat() (float64, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
 
-func Pswap(f *int, s *int){
-	temp:=*f
-	*f=*s
-	*s=temp
-	fmt.Println("Pointer Swap\nfirst :",*f,"second :",*s)
+	input = strings.TrimSpace(input)
+	number, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, err
+	}
+	return number, nil
 }
-
