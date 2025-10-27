6eea5123f61323e7ab2eaa5a43e819659e2c5546
ShiitakeMushroomCode <202144092@itc.ac.kr>
2025-10-27 14:27:27 +0900

    func/error

--- week09/func/main.go
+++ week09/func/main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 func main(){
-	fmt.Print(floatParts(5.1))
+	liters, err := PaintCalc(-5.0, 10.0)
+  if err != nil {
+    fmt.Println("Error:", err) 
+    return
+  }
+  fmt.Printf("%0.2f liters needed\n", liters)
 }
-func floatParts(number float64) (iP int, fP float64){
-	wN := math.Floor(number)
-	return int(wN), number - wN
+func PaintCalc(w float64, h float64) (float64, error) { 
+  if w <= 0 {
+    return 0, fmt.Errorf("가로 값이 0이하 입니다. %.2f", w) 
+  }
+  if h <= 0 {
+    return 0, fmt.Errorf("세로 값이 0이하 입니다. %.2f", h) 
+  }
+  
+  liters := w * h / 10
+  return liters, nil 
 }
