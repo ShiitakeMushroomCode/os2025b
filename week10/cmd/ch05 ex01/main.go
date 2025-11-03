0e3a93cd27f8bc8d931a6f170e6a991892f8191d
ShiitakeMushroomCode <202144092@itc.ac.kr>
2025-11-03 15:44:40 +0900

    for range array

--- week10/cmd/ch05 ex01/main.go
+++ week10/cmd/ch05 ex01/main.go
@@ -4,14 +4,8 @@ import "fmt"
 
 
 func main() {
-	arrayString := [7]string{"도","레","미","파","솔","라","시"}
-	arrayInt := [3]int{-9, 11, 7}
-	for i := 0; i<len(arrayInt); i++{
-		fmt.Printf("%d번째 arrayInt의 값: %d\n", i+1, arrayInt[i])
+	numbers := [3]int{-9, 11, 7}
+	for i, number := range numbers{
+		fmt.Println(i, number)
 	}
-	for i := 0; i<len(arrayString); i++{
-		fmt.Printf(arrayString[i])
-	}
-	fmt.Printf("#v\n", arrayInt)
-	fmt.Printf("#v\n", arrayString)
 }
