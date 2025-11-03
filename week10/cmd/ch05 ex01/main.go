d5b47a5c20d8d2d7e132d59be2d06ac4725698a1
ShiitakeMushroomCode <202144092@itc.ac.kr>
2025-11-03 15:31:46 +0900

    literal 2

--- week10/cmd/ch05 ex01/main.go
+++ week10/cmd/ch05 ex01/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 
 func main() {
-	var arrayBool [3]bool = [3]bool{false,true,false}
+	arrayBool := [3]bool{false,true,false}
 	var arrayInt [3]int
 	fmt.Println(arrayBool[1])
 	fmt.Println(arrayInt[0])
