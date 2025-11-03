6f7ca08e099c778abd4a9e54f645ab74595072f7
ShiitakeMushroomCode <202144092@itc.ac.kr>
2025-11-03 15:24:42 +0900

    go array

---
+++ week10/cmd/ch05 ex01/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	var arrayBool [3]bool
+	var arrayInt [3]int
+	fmt.Println(arrayBool[2])
+	arrayInt[1]++
+	arrayInt[1] = arrayInt[1] + 1
+	fmt.Println(arrayInt[1])
+}
