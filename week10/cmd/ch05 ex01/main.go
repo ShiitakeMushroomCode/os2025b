07a4bb36dd3e52ed9f40deb1342bae8e3c93260a
ShiitakeMushroomCode <202144092@itc.ac.kr>
2025-11-03 15:36:10 +0900

    array for

--- week10/cmd/ch05 ex01/main.go
+++ week10/cmd/ch05 ex01/main.go
@@ -4,8 +4,14 @@ import "fmt"
 
 
 func main() {
-	arrayBool := [3]bool{false,true,false}
-	var arrayInt [3]int
-	fmt.Println(arrayBool[1])
-	fmt.Println(arrayInt[0])
+	arrayString := [7]string{"도","레","미","파","솔","라","시"}
+	arrayInt := [3]int{-9, 11, 7}
+	for i := 0; i<len(arrayInt); i++{
+		fmt.Printf("%d번째 arrayInt의 값: %d\n", i+1, arrayInt[i])
+	}
+	for i := 0; i<len(arrayString); i++{
+		fmt.Printf(arrayString[i])
+	}
+	fmt.Printf("#v\n", arrayInt)
+	fmt.Printf("#v\n", arrayString)
 }
