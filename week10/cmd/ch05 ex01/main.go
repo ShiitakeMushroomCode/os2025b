4be00769a565a4cafa8716d18d6387cd90a8cfc3
ShiitakeMushroomCode <202144092@itc.ac.kr>
2025-11-03 15:30:13 +0900

    array literal

--- week10/cmd/ch05 ex01/main.go
+++ week10/cmd/ch05 ex01/main.go
@@ -4,10 +4,8 @@ import "fmt"
 
 
 func main() {
-	var arrayBool [3]bool
+	var arrayBool [3]bool = [3]bool{false,true,false}
 	var arrayInt [3]int
-	fmt.Println(arrayBool[2])
-	arrayInt[1]++
-	arrayInt[1] = arrayInt[1] + 1
-	fmt.Println(arrayInt[1])
+	fmt.Println(arrayBool[1])
+	fmt.Println(arrayInt[0])
 }
