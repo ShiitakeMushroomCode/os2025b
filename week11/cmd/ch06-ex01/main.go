f38564deba98a5d0b6588b5ae025c8f0022b8809
ShiitakeMushroomCode <202144092@itc.ac.kr>
2025-11-10 14:22:19 +0900

    slice

---
+++ week11/cmd/ch06-ex01/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var subj []string
+	subj = make([]string, 3)
+	subj[0] = "Go"
+	subj[2] = "Python"
+
+	for _, sub := range subj{
+		fmt.Println(sub)
+	}
+}
