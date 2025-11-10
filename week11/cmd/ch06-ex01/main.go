38cc8b9355942bcf0810788a53b93be4471d3fb9
ShiitakeMushroomCode <202144092@itc.ac.kr>
2025-11-10 14:26:17 +0900

    slice literal

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -5,10 +5,10 @@ import "fmt"
 func main() {
 	// var subj []string
 	// subj = make([]string, 3)
-	subj := make([]string, 3)
-	subj[0] = "Go"
-	subj[2] = "Python"
-
+	// subj := make([]string, 3)
+	// subj[0] = "Go"
+	// subj[2] = "Python"
+	subj := []string{"Go","","Python"}
 	for _, sub := range subj{
 		fmt.Println(sub)
 	}
