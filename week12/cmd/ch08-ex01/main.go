5bda7fadd9e4cb908b696c4c9680021368617c39
ShiitakeMushroomCode <202144092@itc.ac.kr>
2025-11-17 14:22:40 +0900

    map

--- week12/cmd/ch08-ex01/main.go
+++ week12/cmd/ch08-ex01/main.go
@@ -1,4 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"log"
 
-func main() {}
+	"github.com/headfirstgo/datafile"
+)
+
+
+func main() {
+	counts := make(map[string]int)
+	lines, err := datafile.GetStrings("votes.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, line := range lines {
+		counts[line]++
+	}
+	for name, count := range counts {
+		fmt.Println(name, ":", count)
+	}
+}
