f50aeef039732dc9e7eea7c3b8ca1c1753fa8892
ShiitakeMushroomCode <202144092@itc.ac.kr>
2025-11-17 15:35:37 +0900

    type

--- week12/cmd/ch08-ex02/main.go
+++ week12/cmd/ch08-ex02/main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"fmt"
 
-func main() {}
+	"github.com/headfirstgo/magazine"
+)
+
+
+func main() {
+	var s1 magazine.Subscriber
+	s1.Name = "Mark"
+	s1.Rate = 5.6
+	fmt.Println(s1)
+
+	var e1 magazine.Employee
+	e1.Name = "John"
+	e1.Salary = 15000
+	e1.Address.City = "Incheon"
+	fmt.Println(e1)
+}
